c5f2859f33b7f8a028f7ecf1bae3c29f74c8f8cf
Yuliya Zhak <yulia@Yuliyas-MacBook-Air.local>
2018-02-07 16:41:44 -0500

    Add user authentication

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,6 +3,8 @@ require 'factory_bot'
 FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
+    first_name 'Jon'
+    last_name 'Snow'
     password 'password'
     password_confirmation 'password'
   end
