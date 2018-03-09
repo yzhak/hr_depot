1e08a7545c2307f9a8dcc1168373644dda61473e
Yuliya Zhak <yulia@Yuliyas-MacBook-Air.local>
2018-03-08 20:55:01 -0500

    Add SendGrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,9 +39,19 @@
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "localhost",
-    port: 1025
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    address: "smtp.sendgrid.net",
+    port: "587",
+    authentication: "plain",
+    enable_starttls_auto: true
   }
+
+  # config.action_mailer.smtp_settings = {
+  #   address: "localhost",
+  #   port: 1025
+  # }
 
   config.action_mailer.default_url_options = {
     host: "localhost",
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,14 +67,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   # Setup the mailer config
+  config.action_mailer.default_url_options = { host: 'hr-depot.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'hr-depot.herokuapp.com',
+    :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
-    :port => 587,
+    :port => '587',
     :authentication => :plain,
     :enable_starttls_auto => true
   }
